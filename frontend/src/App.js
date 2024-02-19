2cedb7816f067d3e7df0a1f32961d561b5468ae4
VASHNIROY <vashniroynallapu@gmail.com>
2024-02-19 16:01:14 +0530

    carousel

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import ProductsSlider from "./Components/ProductsSlider";
+
+function App() {
+  return <ProductsSlider />;
+}
+
+export default App;
