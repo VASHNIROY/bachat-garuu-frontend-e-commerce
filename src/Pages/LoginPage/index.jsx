364856ee9bc1483dc2a753148714433d73388768
VASHNIROY <vashniroynallapu@gmail.com>
2024-03-11 13:05:30 +0530

    login page integrated to backend

--- src/Pages/LoginPage/index.jsx
+++ src/Pages/LoginPage/index.jsx
@@ -1,6 +1,52 @@
 import { Checkbox } from "@material-ui/core";
 import "./index.css";
+import Cookies from "js-cookie";
+import { useState } from "react";
 const Login = () => {
+  const [login, setLogin] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  console.log(login);
+
+  console.log(password);
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+
+  const submitLoginDetails = async () => {
+    const loginData = {
+      email_or_mobile: login,
+      password: password,
+    };
+
+    console.log(loginData);
+
+    const loginFormData = new FormData();
+
+    Object.entries(loginData).forEach(([key, value]) => {
+      loginFormData.append(key, value);
+    });
+
+    console.log("api", `${baseUrl}login`);
+    try {
+      const response = await fetch(`${baseUrl}login`, {
+        method: "POST",
+        body: loginFormData,
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      // Handle success response from the API
+      Cookies.set("userid", data.user_id);
+      setLogin("");
+      setPassword("");
+    } catch (error) {
+      // Handle error response from the API
+      console.error("Error submitting login details:", error);
+    }
+  };
   return (
     <div className="login-page-main-container">
       <div className="login-page-container1">
@@ -9,17 +55,30 @@ const Login = () => {
           <label className="login-page-label">
             Username or email address *
           </label>
-          <input className="login-page-input" />
+          <input
+            className="login-page-input"
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+          />
         </div>
         <div className="login-page-input-container">
           <label className="login-page-label">Password *</label>
-          <input className="login-page-input" />
+          <input
+            className="login-page-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div className="login-page-checkbox-container">
           <Checkbox className="login-page-checkbox" />
           <label className="login-page-label">Remember me</label>
         </div>
-        <button className="login-page-button">Login</button>
+        <button
+          className="login-page-button"
+          onClick={() => submitLoginDetails()}
+        >
+          Login
+        </button>
         <p className="login-page-text1 login-page-text">Lost your Password?</p>
       </div>
       <div className="login-page-container1">
